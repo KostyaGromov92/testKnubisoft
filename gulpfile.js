508268f90a76c3433ef428911fcc321871ee13f3
DESKTOP-52VJMD4\Костя <kotya@zeoalliance.com>
2017-03-30 12:02:24 +0300

    Add project for block

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,16 @@
 
 var gulp = require('gulp'),
     jade = require('gulp-jade'),
+    affected = require('gulp-jade-find-affected'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     cssnano = require('gulp-cssnano'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    newer = require('gulp-newer'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    uglifycss = require('gulp-uglifycss');
 
 gulp.task('sass', function(){ // Создаем таск Sass
     return gulp.src('app/scss/**/*.scss')
@@ -17,21 +23,36 @@ gulp.task('sass', function(){ // Создаем таск Sass
 });
 
 gulp.task('css-libs', function() {
-    return gulp.src('dist/css/*.css')
-        .pipe(cssnano())
-        .pipe(rename({suffix: '.min'}))
+    return gulp.src(['dist/css/style.css','dist/css/jquery.bxslider.css'])
+        .pipe(concat('style.min.css'))
+        .pipe(uglifycss())
         .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('jade', function() {
-    return gulp.src('app/*.jade')
-        .pipe(jade())
+gulp.task('scripts', function() {
+    return gulp.src([
+        'app/script/script.js', // Берем jQuery
+        'app/script/jquery.bxslider.min.js'
+    ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('jade', function () {
+    return gulp.src(['app/jade/*.jade','!app/jade/_*.jade'])
+        .pipe(newer('app/jade/*.jade'))
+        .pipe(plumber())
+        .pipe(jade({
+            pretty: '\t'
+        }))
+        .on('error', console.log)
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', ['sass', 'jade'], function() {
+gulp.task('watch', ['sass', 'jade','scripts'], function() {
     gulp.watch('app/scss/**/*.scss', ['sass']);
-    gulp.watch('app/*.jade', ['jade']);
+    gulp.watch('app/jade/*.jade', ['jade']);
 });
 
 gulp.task('default', ['watch']);
